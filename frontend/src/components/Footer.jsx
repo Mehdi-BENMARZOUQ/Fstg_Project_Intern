b6c205cdd503e848f226014354fe6e46d05c0c02
MediQ-Solution <mediqsolution@gmail.com>
2025-01-23 19:03:18 +0100

    First commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="main-footer">
+            <div className="footer-left">
+                <span> Mehdi Benmarzouq && Omar Bouddaden</span>
+            </div>
+        </footer>
+    );
+}
